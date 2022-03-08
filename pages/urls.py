7a9b46e6d08cacdbc0cb29b62c7ec6322fa2b00e
Ross Meldrum <rosswmeldrum@gmail.com>
2022-03-08 20:44:27 +0000

    Added new Pages

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import homePageView
+from .views import HomePageView, AboutPageView, RAMPageView, CpuPageView, PsuPageView, GpuPageView,MboardPageView
+
 
 urlpatterns = [
-    path('', homePageView, name='home'),
+    path('about/', AboutPageView.as_view(), name='about'), # new
+    path('', HomePageView.as_view(), name='home'),
+    path('RAM/', RAMPageView.as_view(), name='ram'),
+    path('CPU/', CpuPageView.as_view(), name='cpu'),
+    path('PSU/', PsuPageView.as_view(), name='psu'),
+    path('GPU/', GpuPageView.as_view(), name='gpu'),
+    path('MBOARD/', MboardPageView.as_view(), name='mboard'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,27 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+
+class AboutPageView(TemplateView): # new
+    template_name = 'About.html'
+
+
+class RAMPageView(TemplateView): # new
+    template_name = 'pages/RAM.html'
+
+
+class CpuPageView(TemplateView): # new
+    template_name = 'pages/cpu.html'
+
+
+class PsuPageView(TemplateView): # new
+    template_name = 'pages/psu.html'
+
+
+class GpuPageView(TemplateView): # new
+    template_name = 'pages/gpu.html'
+
+
+class MboardPageView(TemplateView): # new
+    template_name = 'pages/motherboard.html'
